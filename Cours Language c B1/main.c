750527a122b83d5457ae4eacc907fd6f5a084a16
LordPouic <janinloic@gmail.com>
2020-01-05 11:17:10 +0100

    Update main.c

--- Cours Language c B1/main.c
+++ Cours Language c B1/main.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "ExoTableaux.h"
-
 
 int main()
 {
-    printf("Vous pouvez tester vos exercices ici");
-    printf("char : %d octets\n", sizeof(char));
-    printf("int : %d octets\n", sizeof(int));
-    printf("int[100] : %d octets\n", sizeof(int[100]));
 
 }
 
